921b0d62668f609371498776e4790ad7ba4eb1a9
Yiwei221 <67593229+Yiwei221@users.noreply.github.com>
2020-08-02 17:16:49 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    //stack.java test code
     Stack stack = new Stack(3);
+    stack.push(1);
+    System.out.println("Top element: " + stack.peek());
+		System.out.println("Stack size: " + stack.size());
+    stack.push(2);
+    System.out.println("Top element : " + stack.peek());
+		System.out.println("Stack size: " + stack.size());
+    stack.pop();
+    System.out.println("Top element: " + stack.peek());
+		System.out.println("Stack size: " + stack.size());
   }
 }
