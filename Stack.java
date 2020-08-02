5b00d8fcdf7b2783cee9d545446af7abf0e9eb52
Yiwei221 <67593229+Yiwei221@users.noreply.github.com>
2020-08-02 17:05:44 -0400

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -7,12 +7,35 @@ class Stack {
   public Stack(int n){
     list= new int[n];
     capacity = n;
+    size=capacity;
     top = -1;
   }
 
   public void push(int x)
 	{
-		System.out.println("Inserting " + x);
+		if (isFull())
+		{
+			System.out.println("Error");
+			System.exit(1);
+		}
+		System.out.println("insert: " + x);
 		list[++top] = x;
+	}
+
+  public int pop()
+	{
+		if (isEmpty())
+		{
+			System.out.println("Error2");
+			System.exit(1);
+		}
+
+		System.out.println("Remove: " + peek());
+		return list[top--];
+	}
+
+  public int peek()
+	{
+		return list[top];
 	}
 }
