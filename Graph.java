e88a1bcc675484676723d29554eeddaba517fa91
Yiwei221 <67593229+Yiwei221@users.noreply.github.com>
2020-08-02 23:06:16 -0400

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -22,8 +22,7 @@ class Graph{
     while(queue.size!=0){
       int n=queue.pop();
       int i=0;
-      t[0]=n;
-      i+=1;
+      t=add(t,n);
       if(n==goal){
         break;
       }else{
@@ -37,8 +36,33 @@ class Graph{
     return t;
   }
 
-  int[] traverse_dfs(int start){
-    
+  public int[] traverse_dfs(int start, int goal){
+    Stack stack = new Stack(graph[0].length);
+    int[] t=new int[graph[0].length];
+    stack.push(start);
+    while(stack.size!=0){
+      start=stack.pop();
+      t=add(t,start);
+      if(start==goal){
+        break;
+      }else{
+        for(int j=0;j<graph[0].length;j++){
+          if(graph[n][j]==1){
+          stack.push(j);  
+          }
+        }
+      }
+    }
+    return t;
+  }
+
+  public int[] add(int array[], int element){
+    int[] newA=new int[array.length+1];
+    for (int i = 0; i < array.length+1; i++){
+      newA[i] = array[i];
+    } ;
+    newA[array.length+1]=element;
+    return newA;
   }
 
 }
