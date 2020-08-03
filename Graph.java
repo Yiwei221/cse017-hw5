e4f30f11023aa19c64f73ab6d69449e2a591ed79
Yiwei221 <67593229+Yiwei221@users.noreply.github.com>
2020-08-02 21:30:19 -0400

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Iterator;
+
 class Graph{
   int[][] graph;
 
@@ -7,4 +10,35 @@ class Graph{
   public void add_edge(int from, int to){
     graph[from-1][to-1]=1;
   }
+
+  public void remove_edge(int from, int to){
+    graph[from-1][to-1]=0;
+  }
+
+  public int[] traverse_bfs(int start, int goal){
+    Queue queue = new Queue(graph[0].length);
+    int[] t=new int[graph[0].length];
+    queue.push(start); 
+    while(queue.size!=0){
+      int n=queue.pop();
+      int i=0;
+      t[0]=n;
+      i+=1;
+      if(n==goal){
+        break;
+      }else{
+        for(int j=0;j<graph[0].length;j++){
+          if(graph[n][j]==1){
+          queue.push(j);  
+          }
+        }
+      }
+    }
+    return t;
+  }
+
+  int[] traverse_dfs(int start){
+    
+  }
+
 }
