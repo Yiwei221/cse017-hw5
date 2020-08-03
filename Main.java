8f60ef1a74c55f8692550d2523d9b142ca3157ed
Yiwei221 <67593229+Yiwei221@users.noreply.github.com>
2020-08-02 20:31:23 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,13 +6,13 @@ class Main {
     Stack stack = new Stack(3);
     stack.push(1);
     System.out.println("Top element: " + stack.peek());
-		System.out.println("Stack size: " + stack.size());
+    System.out.println("Stack size: " + stack.size());
     stack.push(2);
     System.out.println("Top element : " + stack.peek());
-		System.out.println("Stack size: " + stack.size());
+    System.out.println("Stack size: " + stack.size());
     stack.pop();
     System.out.println("Top element: " + stack.peek());
-		System.out.println("Stack size: " + stack.size());
+    System.out.println("Stack size: " + stack.size());
 
     System.out.println("Queue: ");
     int size = 10;
