a19505543ca071a7123066fd302fc5352f4bffba
Yiwei221 <67593229+Yiwei221@users.noreply.github.com>
2020-08-02 20:31:04 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 class Main {
   public static void main(String[] args) {
     //stack.java test code
+    System.out.println("Stack: ");
+
     Stack stack = new Stack(3);
     stack.push(1);
     System.out.println("Top element: " + stack.peek());
@@ -11,5 +13,21 @@ class Main {
     stack.pop();
     System.out.println("Top element: " + stack.peek());
 		System.out.println("Stack size: " + stack.size());
+
+    System.out.println("Queue: ");
+    int size = 10;
+    Queue queue = new Queue(size);
+
+    queue.push(5);
+    System.out.println("Top elment: " + queue.peek());
+    queue.push(10);
+    System.out.println("Top elment: " + queue.peek());
+    queue.push(20);
+    System.out.println("Top elment: " + queue.peek());
+    queue.push(40);
+    System.out.println("Top elment: " + queue.peek());
+    queue.pop();
+    System.out.println("Top elment: " + queue.peek());
+
   }
 }
